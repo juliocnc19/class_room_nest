4fe41c2a61adafcd700462742370b90e8fb78aba
juliocnc19 <juliocnietoc19@gmail.com>
2024-10-27 00:13:08 -0400

    [ADD] avances en modulo de usuarios

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('/create')
+  async create(@Body() userCreateInput: Prisma.UserCreateInput) {
+    return await this.usersService.create(userCreateInput);
+  }
+
+  @Get('/find/:email')
+  async find(@Param('email') email: string) {
+    return await this.usersService.find(email);
+  }
+
+  @Get('/findMany')
+  async findMany() {
+    return await this.usersService.findMany();
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User, Prisma } from '@prisma/client';
+import { Users } from './users.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService implements Users {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prismaService.user.create({ data });
+  }
+
+  async find(email: string): Promise<User> {
+    return this.prismaService.user.findUnique({ where: { email } });
+  }
+
+  async findMany(): Promise<User[]> {
+    return this.prismaService.user.findMany();
+  }
+}
