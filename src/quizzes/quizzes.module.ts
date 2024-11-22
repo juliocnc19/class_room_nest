38c2bbe0a7c6723359976522cc360693cb1640b3
juliocnc19 <juliocnietoc19@gmail.com>
2024-11-21 20:51:54 -0400

    [ADD] error de dependencias

--- src/quizzes/quizzes.module.ts
+++ src/quizzes/quizzes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QuizzesService } from './quizzes.service';
 import { QuizzesController } from './quizzes.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [QuizzesController],
   providers: [QuizzesService],
 })
